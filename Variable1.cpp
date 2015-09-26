afb713fa84f93b734f9bd533b81ad74f463349cc
譛ｨ荳玖恭讓ｹ <kashima-antlers.-_-.kinopy@i.softbank.jp>
2015-09-26 22:21:44 +0900

    新規作成

---
+++ Variable1.cpp
@@ -0,0 +1,22 @@
+//
+//  Variable1.cpp
+//  RobelCpp
+//
+//  Created by 木下 英樹 on 2015/09/26.
+//  Copyright © 2015年 木下 英樹. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    
+    a = 0;
+    cout << "a = " << a << endl;
+    
+    a = 5;
+    cout << "2a = " << a * 2 << endl;
+    
+    //return 0;
+}
