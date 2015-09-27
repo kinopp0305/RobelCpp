374bed5db9a3b2f5df599353bd7dd2962b3b4f4d
譛ｨ荳玖恭讓ｹ <kashima-antlers.-_-.kinopy@i.softbank.jp>
2015-09-27 10:53:44 +0900

    新規作成

---
+++ 02-10_Array1/Array1.cpp
@@ -0,0 +1,25 @@
+//
+//  Array1.cpp
+//  RobelCpp
+//
+//  Created by 木下 英樹 on 2015/09/27.
+//  Copyright © 2015年 木下 英樹. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[2];
+    
+    a[0] = 100;
+    cout << "a[0] = " << a[0] << endl;
+    
+    a[1] = 200;
+    cout << "a[0] = " << a[0] << endl;
+    
+    a[0] = 300;
+    cout << "a[0] = " << a[0] << endl;
+    
+    return 0;
+}
