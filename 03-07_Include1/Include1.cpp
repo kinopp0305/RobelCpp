5acd02f1a9411d175c5852347f57edb8e6539a7f
譛ｨ荳玖恭讓ｹ <kashima-antlers.-_-.kinopy@i.softbank.jp>
2015-09-27 12:04:16 +0900

    新規作成

---
+++ 03-07_Include1/Include1.cpp
@@ -0,0 +1,21 @@
+//
+//  Include1.cpp
+//  RobelCpp
+//
+//  Created by 木下 英樹 on 2015/09/27.
+//  Copyright © 2015年 木下 英樹. All rights reserved.
+//
+
+#include <iostream>
+#include "Include1.h"
+using namespace std;
+
+int main() {
+    Hello();
+    return 0;
+}
+
+void Hello() {
+    cout << "やぁ、こんちは。" << endl;
+    //return 0;
+}
