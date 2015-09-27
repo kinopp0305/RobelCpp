b864a90ed9e324f8706c152ff38ddaa7ad774d0c
譛ｨ荳玖恭讓ｹ <kashima-antlers.-_-.kinopy@i.softbank.jp>
2015-09-27 10:46:27 +0900

    新規作成

---
+++ 02-09_For1/For1.cpp
@@ -0,0 +1,19 @@
+//
+//  For1.cpp
+//  RobelCpp
+//
+//  Created by 木下 英樹 on 2015/09/27.
+//  Copyright © 2015年 木下 英樹. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int i;
+    
+    for(i = 0; i < 10; i++) {
+        cout << "**********" << endl;
+    }
+    return 0;
+}
