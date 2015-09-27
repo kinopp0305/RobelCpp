54d8303e10c923606525ba8a1cd69ed0a20e9b40
譛ｨ荳玖恭讓ｹ <kashima-antlers.-_-.kinopy@i.softbank.jp>
2015-09-27 09:38:29 +0900

    リファクタリング

--- 02-03_Func1/Func1.cpp
+++ 02-03_Func1/Func1.cpp
@@ -9,13 +9,11 @@
 #include <iostream>
 using namespace std;
 
-int f(int x, int y)
-{
+int f(int x, int y) {
     return 2 * x + y;
 }
 
-int main()
-{
+int main(int argc, const char * argv[]) {
     cout << "f(1, 2) = " << f(1, 2) << endl;
     cout << "f(182, 144) = " << f(182, 144) << endl;
     
