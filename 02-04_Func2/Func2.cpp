b7e200b6e89f0cd6d9defe61dbd8bbc26202931a
譛ｨ荳玖恭讓ｹ <kashima-antlers.-_-.kinopy@i.softbank.jp>
2015-09-27 09:27:48 +0900

    新規作成

---
+++ 02-04_Func2/Func2.cpp
@@ -0,0 +1,20 @@
+//
+//  Func2.cpp
+//  RobelCpp
+//
+//  Created by 木下 英樹 on 2015/09/27.
+//  Copyright © 2015年 木下 英樹. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+void Hello() {
+    cout << "やぁ、こんちは。" << endl;
+}
+
+int main(int argc, const char * argv[]) {
+    Hello();
+    Hello();
+    return 0;
+}
